e8659317ef7b4ce5d10e9e30c56215e997f16ee5
beaverDamDemo <zdenevi@gmail.com>
2026-09-20 20:04:59 +0200

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,11 +4,13 @@ import styles from './not-found.module.css';
 export default function NotFound() {
   return (
     <main className={styles.page}>
+      <div className={styles.imageArea}>
         <nav aria-label="Not found page actions">
           <Link className={styles.primaryAction} href="/">
             Return Home
           </Link>
         </nav>
+      </div>
     </main>
   );
 }
